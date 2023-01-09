4609b9f8e18280a7e1d99356665afeff71aea233
RyndyukDanila <ryndyukdanila@gmail.com>
2023-01-09 15:53:13 +0600

    front-end

---
+++ main.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="static/css/style.css">
+		<title>Dogs Shelter</title>
+	</head>
+	<body>
+		<div class="wrapper">
+			<header id="open" class="header">
+				<div id="close" class="header__container">	
+					<a href="" class="header__logo">
+						<img src="static/img/logo.svg" width=40px alt="Логотип">
+					</a>
+					<div class="header__menu menu">
+						<nav class="menu__body">
+							<ul class="menu__list">
+								<li class="menu__item"><a href="" class="menu__link">ГЛАВНАЯ</a></li>
+								<li class="menu__item"><a href="" class="menu__link">ПОРОДЫ</a></li>
+								<li class="menu__item"><a href="" class="menu__link">ОБЩИЙ СПИСОК</a></li>
+								<li class="menu__item"><a href="" class="menu__link">ПРОФИЛЬ</a></li>
+							</ul>
+						</nav>
+					</div>
+					<div class="header__button">
+						<a href="" class="button button_blue">ВОЙТИ</a>
+						<a href="#open" class="icon-menu"><span></span></a>
+						<a href="#close" class="close-icon-menu"></a>
+					</div>
+				</div>
+			</header>
+
+			<main class="main">
+				<section class="get-started">
+					<div class="get-started__container">
+						<div class="get-started__content">
+							<div class="get-started__block-text block-text">
+								<h1 class="block-text__title block-text__title_blue">Твой <span>питомец</span> бесценен.</h1>
+								<div class="block-text__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum illum ab similique aperiam adipisci, quo.</div>
+								<a href="" class="block-text__button button">НАЧАТЬ</a>
+							</div>
+						</div>
+						<div class="get-started__image">
+							<img src="static/img/get-started/blue-vec.svg" alt="Картинка собаки и ее владельца">
+						</div>
+						<div class="get-started__video">
+							<a href="" class=" video-get-started">
+								<div class="video-get-started-icon">
+									<img src="static/img/get-started/play.svg" width="44px" alt="Кнопка Play">
+								</div>
+								<div class="video-get-started__body">
+									<div class="video-get-started__title">Правильно ухаживай за своим питомцем</div>
+									<div class="video-get-started__text">Посмотреть видео</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</section>
+				<section class="dog-breed">
+					<div class="dog-breed__container">
+						<div class="dog-breed__image">
+							<div class="image-dog-breed">
+								<div class="image-dog-breed__image">
+									<img src="static/img/dog-breed/blue-dog-breed.svg" alt="Картинка собаки в ветклинике">
+								</div>
+								<div class="image-dog-breed__item image-dog-breed__item_1"></div>
+								<div class="image-dog-breed__item image-dog-breed__item_2"></div>
+								<div class="image-dog-breed__item image-dog-breed__item_3"></div>
+							</div>
+						</div>
+						<div class="dog-breed__content">
+							<div class="dog-breed__block-text block-text">
+								<h2 class="block-text__title">Узнай больше о породе своей собаки.</h2>
+								<div class="block-text__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, nisi magni fugiat dolore.</div>
+								<a href="" class="block-text__button button">ПОРОДЫ</a>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
+
+			<footer class="footer">
+				<div class="footer__container">
+					<div class="footer__empty"></div>
+					<a href="" class="header__logo">
+						<img src="static/img/logo.svg" width=40px alt="Логотип">
+					</a>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
